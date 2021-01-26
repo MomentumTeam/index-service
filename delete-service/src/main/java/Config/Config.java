39154e1583f44dc5e88132c562788dfb63d96564
Barak Steindl <steindl.barak@gmail.com>
2021-01-26 16:23:18 +0200

    tiny changes

--- delete-service/src/main/java/Config/Config.java
+++ delete-service/src/main/java/Config/Config.java
@@ -22,12 +22,6 @@ public class Config {
     public static final String ERROR_QUEUE_NAME = (System.getenv("ERROR_QUEUE_NAME")!=null) ? System.getenv("ERROR_QUEUE_NAME") : "error";
     public static final String  ERROR_ROUTING_KEY = (System.getenv("ERROR_ROUTING_KEY")!=null) ? System.getenv("ERROR_ROUTING_KEY") : "errorKey" ;
 
-    public static final HashSet<MessageEvent> DELETE_EVENTS = new HashSet<MessageEvent>() {{
-        add(MessageEvent.CONTENT_CHANGE);
-        add(MessageEvent.DELETE);
-    }};
-
-
     public static final String ELASTIC_HOST = (System.getenv("ELASTIC_HOST")!=null) ? System.getenv("ELASTIC_HOST") : "40.127.198.131" ;
     public static final int ELASTIC_PORT = (System.getenv("ELASTIC_PORT")!=null) ? Integer.parseInt(System.getenv("ELASTIC_PORT")) : 9200;
     public static final String ELASTIC_PROTOCOL = (System.getenv("ELASTIC_PROTOCOL")!=null) ? System.getenv("ELASTIC_PROTOCOL") : "http";
